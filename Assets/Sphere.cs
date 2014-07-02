c37b203a78f1f270fcf21e79ae5939f347e6cb74
shunnjp <shunnjp@gmail.com>
2014-07-02 19:52:41 +0900

    Added unity project data

---
+++ Assets/Sphere.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sphere : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate (new Vector3(-0.05f, 0.0f, 0.0f));
+	}
+
+	void OnTriggerEnter2D(Collider2D c){
+		Debug.Log ("Collision");
+		//enabled = false;
+	}
+}
