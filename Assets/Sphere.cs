69ea6c662ee0679f4ac7e7b50b3eb171267f08c1
shunnjp <shunnjp@gmail.com>
2014-07-08 20:39:16 +0900

    Modified sphere's behavior

--- Assets/Sphere.cs
+++ Assets/Sphere.cs
@@ -11,10 +11,20 @@ public class Sphere : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		gameObject.transform.Translate (new Vector3(-0.05f, 0.0f, 0.0f));
+		if(gameObject.transform.position.x < -3){
+			gameObject.transform.position = new Vector3(4, 0, -13);
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D c){
-		Debug.Log ("Collision");
+		Debug.Log ("CollisionEnter");
 		//enabled = false;
+	}
+
+	void OnTriggerStay2D(Collider2D c){
+		Debug.Log ("CollisionStay");
+	}
+	void OnTriggerExit2D(Collider2D c){
+		Debug.Log ("CollisionExit");
 	}
 }
