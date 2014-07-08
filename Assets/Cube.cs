ae2c4902f9051667a235c60e87735a728c37fa58
shunnjp <shunnjp@gmail.com>
2014-07-08 21:14:40 +0900

    Added Cube's behavior

---
+++ Assets/Cube.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+	float r = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(0.8f, Mathf.Sin (r * Mathf.PI / 180), -15f);
+		r += 5.0f;
+		if(r > 360.0f){
+			r = 0.0f;
+		}
+
+	}
+}
