00ca793f924232b1d88cfc1987d3239701ae18cc
shunnjp <shunnjp@gmail.com>
2014-07-08 21:17:17 +0900

    Modified motion

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -12,7 +12,7 @@ public class Cube : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.position = new Vector3(0.8f, Mathf.Sin (r * Mathf.PI / 180), -15f);
-		r += 5.0f;
+		r += 2.0f;
 		if(r > 360.0f){
 			r = 0.0f;
 		}
